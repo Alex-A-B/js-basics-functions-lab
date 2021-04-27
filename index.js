83dd1a4565bd8cf0472042f5be17d3a8c0f02859
Alex-A-B <alexjamesbelton@gmail.com>
2021-04-27 16:23:59 +0100

    commit tree 2d14d93e140350821bbd44d80ca7fe9693ffce89

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNum) {
+    return Math.abs(blockNum - 42);
+}
+
+function distanceFromHqInFeet(blockNum) {
+    return  distanceFromHqInBlocks(blockNum) * 264;
+}
+
+function distanceTravelledInFeet(startBlock, endBlock) {
+    return Math.abs(startBlock - endBlock) * 264;
+}
+
+function calculatesFarePrice(startBlock, endBlock) {
+    const distance = distanceTravelledInFeet(startBlock, endBlock);
+
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000){
+        return .02 * (distance - 400);
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
